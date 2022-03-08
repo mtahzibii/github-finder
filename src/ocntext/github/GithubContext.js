eeba46a099456c25d4ce4e24d1d3a7c236844f77
mohammadreza tahzibi <m.r.tahzibi@gmail.com>
2022-03-08 04:48:47 -0800

    added user search component./n implemented clear button functionality
    in this commit, component UserSearch created and made it possible to make query string to Github API and find specific users. also clear user list is implemeted.

--- src/ocntext/github/GithubContext.js
+++ src/ocntext/github/GithubContext.js
@@ -2,9 +2,10 @@ import { createContext, useState } from "react";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+// const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
+// Component GithubProveider
 export const GithubProvider = ({ children }) => {
   // Initialize users
   const [users, setUsers] = useState([]);
@@ -13,7 +14,8 @@ export const GithubProvider = ({ children }) => {
 
   // Fetch Data (using async/await)
   const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}`, {
+    // const response = await fetch(`https://api.github.com/search/users?q=brad`, {
+    const response = await fetch(`https://api.github.com/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
@@ -25,8 +27,38 @@ export const GithubProvider = ({ children }) => {
     setLoading(false);
   };
 
+  // Search users from Github API
+  const searchUsers = async (text) => {
+    setLoading(true);
+
+    // Set  URL search Parameters
+    const params = new URLSearchParams({
+      q: text,
+    });
+    // Make a string query (using fetch API)
+    const response = await fetch(
+      `https://api.github.com/search/users?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    );
+    // Get the items value from output as data
+    const { items } = await response.json();
+    // update users after fetch from github API
+    setUsers(items);
+    // Set loading to false (users are loaded)
+    setLoading(false);
+  };
+
+  // Clear users from state
+  const clearUsers = () => setUsers([]);
+
   return (
-    <GithubContext.Provider value={{ users: users, loading, fetchUsers }}>
+    <GithubContext.Provider
+      value={{ users, loading, fetchUsers, searchUsers, clearUsers }}
+    >
       {children}
     </GithubContext.Provider>
   );
